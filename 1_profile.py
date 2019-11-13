d9db1e4b96d689d5cf8fe12a5d09fa6927a9ee07
Xu Ren <musicalxu@hotmail.com>
2019-11-13 08:24:27 +0100

    a simple flask

---
+++ 1_profile.py
@@ -0,0 +1,16 @@
+# rules of optimization
+# 1 - DON'T it's probably fast enough
+# 2 - Don't yet, developer time is expensive, it may be worth just ugprading the hardware
+# 3 - Profile first, then optimize
+
+## TIPS
+# use real data when profiling
+# run on production like environment
+# turn off anti-virus
+# have a test suite
+# known when to measure - be aware of warm up time
+
+# look at entire system and identify bottlenecks
+# take a lookt at the strace
+
+
